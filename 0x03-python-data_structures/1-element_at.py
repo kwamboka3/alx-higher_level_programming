3f7ab26c919e83fe83f4aa642997493aaaa3521d
kwamboka3 <pkwamboka@flag42.com>
2022-06-07 16:14:07 +0300

    create 1-element-at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
-    if (idx < 0):
+    if idx > (len(my_list) - 1) or idx < 0:
         return None
-    elif (idx > (len(my_list) - 1):
-            return None
-    return (my_list[idx])
+    else:
+        return my_list[idx]
