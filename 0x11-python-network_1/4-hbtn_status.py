3d792f570582537e367f610a7e2959b8cb302024
kwamboka3 <pkwamboka@flag42.com>
2022-09-30 12:18:35 +0300

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
+'''network module'''
+
+
+import requests
+
+
 if __name__ == '__main__':
-    import requests
-    r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:\n\t- type: {}\n\t- content: {}'.
+          format(type(response.text), response.text))
