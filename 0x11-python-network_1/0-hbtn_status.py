de429a46dfbf44063dedd169e0a4a3c5c12b6dc5
kwamboka3 <pkwamboka@flag42.com>
2022-09-30 12:07:10 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
+'''network module'''
 import urllib.request
 
-if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        html_str = html.decode('utf-8')
 
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html_str))
+with urllib.request.urlopen('https://intranet.hbtn.io/status')as response:
+    html = response.read()
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+      .format(type(html), html, html.decode()))
