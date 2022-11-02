176c810f2d05823d217071f0a15032ad5c730db8
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 11:00:31 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
-<<<<<<< HEAD
-.const fs = require('fs');
-=======
 const fs = require('fs');
->>>>>>> 60f7ef8dab6c24490730d534863380e09782bb79
 request.get(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
