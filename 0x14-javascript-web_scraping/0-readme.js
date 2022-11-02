22981a1c142d602b7c9e9577ccc9b87adb387d53
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 09:07:35 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
+/*
+# Write a script that reads and prints the content of a file.
+The first argument is the file path
+*/
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
