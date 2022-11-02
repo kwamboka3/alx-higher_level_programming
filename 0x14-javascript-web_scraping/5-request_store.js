4345d5fff4ecfd94da8faf3d68e4cda7ec7dfd6e
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 10:22:28 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,12 +4,10 @@
  * The first argument is the URL to request
  * The second argument the file path to store the body response
  */
-
 const axios = require('axios');
 const fs = require('fs');
 const url = process.argv[2];
 const nameFile = process.argv[3];
-
 axios.get(url)
   .then(function (response) {
     fs.writeFile(nameFile, response.data, 'utf-8', function (err, data) {
