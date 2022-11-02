60f7ef8dab6c24490730d534863380e09782bb79
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-11-02 10:52:23 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-/*
- * Write a script that gets the contents of a webpage and stores it in a file.
- * The first argument is the URL to request
- * The second argument the file path to store the body response
- */
 const request = require('request');
 const fs = require('fs');
 request.get(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
