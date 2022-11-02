2bc2b2f52e80f9dca999e2feda0ddd182d85699d
kwamboka3 <87804803+kwamboka3@users.noreply.github.com>
2022-11-02 10:25:49 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-let characters;
-const dict = {};
-request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (res.statusCode === 200) {
-    characters = JSON.parse(body).characters;
-    for (const url of characters) {
-      request.get(url, function (err1, res1, body1) {
-        if (res1.statusCode === 200) {
-          savedData(url, JSON.parse(body1).name);
-        } else if (err1) {
-          console.log(err1);
-        }
+/*
+ * Write a script that prints all characters of a Star Wars movie:
+ * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * Display one character name by line
+ */
+
+const axios = require('axios');
+const url = ('https://swapi-api.hbtn.io/api/films/');
+const film = process.argv[2];
+
+async function savemovies () {
+  const responses = await axios.get(url + film);
+  const urlName = responses.data.characters;
+  for (let i = 0; i < urlName.length; i++) {
+    const people = urlName[i];
+    await axios.get(people)
+      .then(function (response) {
+        console.log(response.data.name);
       });
-    }
-  } else if (err) {
-    console.log(err);
-  }
-});
-function savedData (url, name) {
-  dict[url] = name;
-  if (Object.entries(dict).length === characters.length) {
-    for (const url of characters) {
-      console.log(dict[url]);
-    }
   }
 }
+savemovies();
