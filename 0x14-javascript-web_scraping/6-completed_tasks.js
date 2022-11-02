1f8a1ac56d7312b2b73193e515b2d0548ae2b4e8
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 11:02:46 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,34 +1,20 @@
 #!/usr/bin/node
-/*
- *Write a script that computes the number of tasks completed by user id.
- *The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
- *Only print users with completed task
- */
-const axios = require('axios');
-const url = process.argv[2];
-let cantidad = 0;
-const dict = {};
-axios.get(url)
-  .then(function (response) {
-    for (const num in response.data) {
-      const userid = response.data[num].userId;
-      const task = response.data[num].completed;
-      if (dict[userid] >= 1) {
-        cantidad = dict[userid];
-      } else {
-        cantidad = 0;
-      }
-      if (task) {
-        cantidad += 1;
-      }
-      dict[userid] = cantidad;
-
-      if (dict[userid] === 0) {
-        delete dict[userid];
+const request = require('request');
+request.get(process.argv[2], function (err, res, body) {
+  const d = {};
+  const total = JSON.parse(body);
+  if (res.statusCode === 200) {
+    for (let i = 0; i < total.length; i++) {
+      if (total[i].completed) {
+        if ((total[i].userId in d)) {
+          d[total[i].userId] += 1;
+        } else {
+          d[total[i].userId] = 1;
+        }
       }
     }
-    console.log(dict);
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
+    console.log(d);
+  } else if (err) {
+    console.log(err);
+  }
+});
