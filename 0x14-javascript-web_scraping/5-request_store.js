ca13c7387637393bd5afb0cc47adcc761c51d289
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 10:53:18 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,5 +5,5 @@
  * The second argument the file path to store the body response
  */
 const request = require('request');
-const fs = require('fs');
+.const fs = require('fs');
 request.get(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
