14aaa8a1a3d157dfac54933de74bcab8b5eb3e81
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 09:26:47 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,7 @@ Write a script that writes a string to a file.
 The first argument is the file path
 The second argument is the string to write
 */
-
 const fs = require('fs');
-
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
