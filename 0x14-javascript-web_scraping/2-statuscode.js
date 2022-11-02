fdeddc654db95c4104c9a0504512fb5951849026
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 09:28:08 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
-request
-  .get(process.argv[2])
-  .on('response', function (response) {
-    console.log('code: ' + response.statusCode);
+/*
+Write a script that display the status code of a GET request.
+The first argument is the URL to request (GET)
+The status code must be printed like this: code: <status code>
+You must use the module axios
+*/
+const axios = require('axios');
+const url = process.argv[2];
+axios.get(url)
+  .then(function (response) {
+    console.log('code: ' + response.status);
+  })
+  .catch(function (err) {
+    console.log('code: ' + err.response.status);
   });
