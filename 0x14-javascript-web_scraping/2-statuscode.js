535bd7a97e71dc15ef78d6b28213f45fc1dbfca7
kwamboka3 <pkwamboka@flag42.com>
2022-11-02 09:33:06 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,10 @@ The first argument is the URL to request (GET)
 The status code must be printed like this: code: <status code>
 You must use the module axios
 */
+
 const axios = require('axios');
 const url = process.argv[2];
+
 axios.get(url)
   .then(function (response) {
     console.log('code: ' + response.status);
